061f1fb367b4981ca3758223d1dd15c7f2d3c982
nebula <jlevidacombe@gmail.com>
2023-12-11 00:50:35 +1300

    refactor

--- src/RPTB-API/RPTB-API/Program.cs
+++ src/RPTB-API/RPTB-API/Program.cs
@@ -2,11 +2,9 @@ using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -15,27 +13,42 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-app.MapGet("/domainstatus/{domain}", async (string domain) => 
+app.MapGet("/domainstatus/{domain}", async (string domain) => await CheckIfDomainOnline(domain))
+    .WithName("GetDomainStatus")
+    .WithOpenApi();
+
+// Add this line to keep the application running.
+await app.RunAsync();
+return;
+
+// This is the extracted method.
+async Task<string> CheckIfDomainOnline(string domain)
 {
     try
     {
         var ping = new Ping();
         var result = await ping.SendPingAsync(domain, 1000);
+        await Task.Delay(1000); // delay was moved inside the extracted method.
         return result.Status == IPStatus.Success ? "Online" : "Offline";
     }
-    catch(Exception ex)
+    catch (PingException)
     {
-        return ex is PingException or ArgumentException or InvalidOperationException or SocketException ? "Offline" : "Error occurred while trying to ping domain";
+        return "Offline";
     }
-	finally
-	{
-        // Set a delay here to prevent the app from terminating immediately.
-        await Task.Delay(1000);
-	}
-})
-.WithName("GetDomainStatus")
-.WithOpenApi();
-
-// Add this line to keep the application running.
-await app.RunAsync();
+    catch (ArgumentException)
+    {
+        return "Offline";
+    }
+    catch (InvalidOperationException)
+    {
+        return "Offline";
+    }
+    catch (SocketException)
+    {
+        return "Offline";
+    }
+    catch (Exception)
+    {
+        return "Error occurred while trying to ping domain";
+    }
+}
