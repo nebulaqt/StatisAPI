217245473fa8b3884be468bf0b8c165d2fdd3cb9
nebula <jlevidacombe@gmail.com>
2023-12-11 06:49:38 +1300

    Refactor exception handling and clean up Program.cs
    This commit considerably cleans up the Program.cs file by reducing redundancy and improving exception handling. The numerous catch blocks have been replaced by a single block that calls a new method, HandleExceptions(ex), which determines the type of exception and returns the appropriate response. Unnecessary comments and delays have also been removed.

--- src/RPTB-API/RPTB-API/Program.cs
+++ src/RPTB-API/RPTB-API/Program.cs
@@ -2,7 +2,6 @@ using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
@@ -12,43 +11,28 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.MapGet("/domainstatus/{domain}", async (string domain) => await CheckIfDomainOnline(domain))
-    .WithName("GetDomainStatus")
-    .WithOpenApi();
-
-// Add this line to keep the application running.
+   .WithName("GetDomainStatus")
+   .WithOpenApi();
+    
 await app.RunAsync();
 return;
 
-// This is the extracted method.
 async Task<string> CheckIfDomainOnline(string domain)
 {
     try
     {
         var ping = new Ping();
         var result = await ping.SendPingAsync(domain, 1000);
-        await Task.Delay(1000); // delay was moved inside the extracted method.
         return result.Status == IPStatus.Success ? "Online" : "Offline";
     }
-    catch (PingException)
+    catch (Exception ex)
     {
-        return "Offline";
+        return HandleExceptions(ex);
     }
-    catch (ArgumentException)
-    {
-        return "Offline";
-    }
-    catch (InvalidOperationException)
-    {
-        return "Offline";
-    }
-    catch (SocketException)
-    {
-        return "Offline";
-    }
-    catch (Exception)
-    {
-        return "Error occurred while trying to ping domain";
-    }
+}
+
+string HandleExceptions(Exception ex)
+{
+    return ex is PingException or ArgumentException or InvalidOperationException or SocketException ? "Offline" : "Error occurred while trying to ping domain";
 }
